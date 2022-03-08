09a35b0029dbe1fd44efcde6d77f4a966a5050f3
Muhammad Iqbal Adenan <kumpultugas17@gmail.com>
2022-03-08 13:49:58 +0800

    Create index.php

---
+++ mik-3/landing-page/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ELTIBIZ | HOME</title>
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <!-- FontAwesome -->
+  <link rel="stylesheet" href="assets/fontawesome/css/all.min.css">
+  <!-- Style -->
+  <link rel="stylesheet" href="assets/style.css">
+</head>
+
+<body>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a href="#" class="navbar-brand">
+        <img src="assets/images/logo.png" alt="Logo_Eltibiz">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a href="#" class="nav-link">Home</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Service</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Course</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Testimonial</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div class="ms-auto">
+        <button type="button" class="btn btn-outline-primary">Login</button>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Header -->
+  <header class="masthead" id="home">
+    <div class="container-fluid px-4 px-lg-5 h-100">
+      <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+        <div class="col-lg-10 align-self-end" style="margin-top: 170px;">
+          <h1 class="text-white display-4 fw-bold">SOLUSI TERBAIK BELAJARMU</h1>
+          <hr class="divider my-5">
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Main -->
+  <!-- Footer -->
+
+  <!-- JavaScript -->
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
