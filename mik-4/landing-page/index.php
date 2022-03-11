20674acb9e05d613a9d00e68bc5beee4e5cab004
Muhammad Iqbal Adenan <kumpultugas17@gmail.com>
2022-03-11 16:30:46 +0800

    Create index.php

---
+++ mik-4/landing-page/index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ELTIBIZ | HOME</title>
+  <!-- Style -->
+  <link rel="stylesheet" href="assets/style.css">
+  <!-- FontAwesome -->
+  <link rel="stylesheet" href="assets/fontawesome/css/all.css">
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+
+<body>
+  <!-- Navigasi -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a href="#home" class="navbar-brand">
+        <img src="assets/images/logo.png" alt="logo eltibiz">
+      </a>
+      <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle Navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarContent">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a href="#home" class="nav-link">Home</a>
+          </li>
+          <li class="nav-item">
+            <a href="#head-service" class="nav-link">Service</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Course</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Testimonial</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div class="ms-auto">
+        <button type="button" class="btn btn-outline-primary">Login</button>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Header -->
+  <header id="home" class="background">
+    <div class="container-fluid px-4 px-lg-5 h-100">
+      <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+        <div class="col-lg-10 align-self-end" style="margin-top: 170px;">
+          <h1 class="text-light display-4 fw-bold">SOLUSI TERBAIK BELAJARMU</h1>
+          <hr class="my-5 divider">
+        </div>
+        <div class="col-lg-8 align-self-baseline">
+          <p class="text-light fs-5 mb-5">
+            Kami menyediakan banyak modul yang kamu butuhkan untuk belajar akuntansi, pemrograman, dan digital marketing dari tingkat dasar hingga mahir.
+          </p>
+          <button class="btn btn-lg btn-primary rounded-pill">Bergabung</button>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Main -->
+  <main>
+    <!-- Service -->
+    <section id="head-service" class="bg-light text-dark">
+      <div class="container-fluid px-3 py-5">
+        <div class="row text-center">
+          <div class="col-12 pb-4">
+            <h1 class="text-dark display-6 text-center mb-5">
+              Mengapa memilih kami ?
+            </h1>
+            <hr class="divider mt-5 mb-3">
+          </div>
+          <div class="col-12 col-md-6 col-lg-3">
+            <span class="fa-stack fa-2x">
+              <i class="fas fa-circle fa-stack-2x text-primary"></i>
+              <i class="fas fa-list-ol fa-stack-1x text-light"></i>
+            </span>
+            <h4>Fasilitas Oke</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+          </div>
+          <div class="col-12 col-md-6 col-lg-3">
+            <span class="fa-stack fa-2x">
+              <i class="fas fa-circle fa-stack-2x text-primary"></i>
+              <i class="fas fa-laptop-code fa-stack-1x text-light"></i>
+            </span>
+            <h4>Modul Lengkap</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+          </div>
+          <div class="col-12 col-md-6 col-lg-3">
+            <span class="fa-stack fa-2x">
+              <i class="fas fa-circle fa-stack-2x text-primary"></i>
+              <i class="fas fa-money-bill fa-stack-1x text-light"></i>
+            </span>
+            <h4>Biaya Terjangkau</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+          </div>
+          <div class="col-12 col-md-6 col-lg-3">
+            <span class="fa-stack fa-2x">
+              <i class="fas fa-circle fa-stack-2x text-primary"></i>
+              <i class="fas fa-chart-line fa-stack-1x text-light"></i>
+            </span>
+            <h4>Terarah</h4>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="service" class="service-bg py-5">
+      <div class="container px-4 py-5">
+        <div class="row justify-content-center">
+          <div class="col-lg-12 text-center">
+            <h1 class="text-light display-6">Kami punya apa yang Anda butuhkan!</h1>
+            <hr class="divider divider-light my-5">
+            <p class="text-light fs-5 mb-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime rem quisquam ea officia quas laudantium voluptates totam, voluptate, aspernatur molestiae assumenda temporibus libero ullam deserunt dolore, aut sint facere ad!
+            </p>
+            <a href="#course" class="btn btn-lg btn-light rounded-pill">Get Started!</a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Course -->
+    <!-- Testimonial -->
+    <!-- Team -->
+  </main>
+
+  <!-- Footer -->
+
+  <!-- JavaScript -->
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
